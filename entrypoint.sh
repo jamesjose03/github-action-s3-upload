770f71a1148d963a673d3dd32f1f122f2ebcfe6b
Rajat Jaic Mendus <jaicmendus@gmail.com>
2020-05-17 15:36:22 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,10 @@ echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
-aws s3 cp ${TO_UPLOAD} s3://${AWS_S3_BUCKET} --region ${AWS_REGION} --recursive
+echo "[default]
+region = ${AWS_REGION}
+output = json" > ~/.aws/config
+
+aws s3 sync . s3://${AWS_S3_BUCKET}/
 
 rm -rf ~/.aws
