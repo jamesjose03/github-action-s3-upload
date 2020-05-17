031ff53a63e313d8b1eef2ef22625b30bfadf82a
Rajat Jaic Mendus <jaicmendus@gmail.com>
2020-05-17 15:22:31 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,6 @@ echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
-aws s3 cp ${TO_UPLOAD} s3://${AWS_S3_BUCKET} \
-            --region ${AWS_REGION} $*
+aws s3 cp ${TO_UPLOAD} s3://${AWS_S3_BUCKET} --region ${AWS_REGION} --recursive
 
 rm -rf ~/.aws
