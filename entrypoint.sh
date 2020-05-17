9f688d9f05e69c9a36b17c3ada978f0c75c436e5
Rajat Jaic Mendus <jaicmendus@gmail.com>
2020-05-17 13:29:00 +0530

    push to s3 action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,15 @@
-#!/bin/sh -l
+#!/bin/sh
 
-echo "Hello Welcome"
-aws help
+set -e
+
+mkdir -p ~/.aws
+touch ~/.aws/credentials
+
+echo "[default]
+aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
+
+aws s3 cp ${FILE} s3://${AWS_S3_BUCKET} \
+            --region ${AWS_REGION} $*
+
+rm -rf ~/.aws
